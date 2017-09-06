e401856b397e37f5dcdf3d7b9dfc544b536abb4a
unidemouser <rahul.niraula@gmail.com>
2017-09-06 12:05:48 +0545

    inheritance

---
+++ src/com/inheritance/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inheritance;
+
+/**
+ *
+ * @author rahul
+ */
+public class MainClass {
+    
+    public static void main(String[] args){
+        Employee e=new Employee("Rahul Niraula", "Biratnagar", "Bachelors", 24, 'm', 30000);
+        e.getFullInformation();
+    }
+}
---
+++ src/com/inheritance/Manager.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inheritance;
+
+/**
+ *
+ * @author rahul
+ */
+public class Manager extends Person {
+    private String boardPosition;
+    private int shares;
+
+    public Manager(String name,String address,String qualification,int age,char gender,String boardPosition,int shares){
+        super(name,address,qualification,age,gender);
+        this.boardPosition = boardPosition;
+        this.shares = shares;
+    }
+    
+    public Manager(String boardPosition, int shares) {
+        this.boardPosition = boardPosition;
+        this.shares = shares;
+    }
+
+    public String getBoardPosition() {
+        return boardPosition;
+    }
+
+    public void setBoardPosition(String boardPosition) {
+        this.boardPosition = boardPosition;
+    }
+
+    public int getShares() {
+        return shares;
+    }
+
+    public void setShares(int shares) {
+        this.shares = shares;
+    }
+    
+    
+}
